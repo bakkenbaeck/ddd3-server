fd9478d02c65e47c92136a6c53915d5a4757bf32
Rens <hi@rensverschuren.com>
2020-01-23 14:42:15 +0100

    Styling, improve stufff

--- app.js
+++ app.js
@@ -12,20 +12,65 @@ import path from 'path';
 // automatically
 dotenv.config();
 
-async function printImage(printer, image) {
+async function printImage(printer, image, counter) {
 
   console.log(chalk.green('Start printing image...'));
 
+  // const date = new Date();
+
+  // date.
+
+
   try {
     printer.clear();
+
     printer.alignCenter();
     printer.println("Bakken & Bæck");
     printer.println("Van Diemenstraat 38");
     printer.println("1013 NH Amsterdam");
-    printer.println("~~~~~~~~~~~");
-    printer.println("You're awesome! :)");
+
+    printer.alignRight();
+
+    printer.newLine();
+
+    printer.println(`23.01.20 20:53`);
+    printer.println(`#${counter}`);
+
+    printer.newLine();
+
+    printer.alignCenter();
+    printer.println("* * * * * * * * * *");
+
+    printer.invert(false);
+
+    // printer.setTypeFontB();
+    // printer.setTextSize(1,1);
+
+    printer.newLine();
+
+    printer.alignLeft();
+    printer.println("“The world is suffering from a dark and silent phenomenon known as ‘digital decay’ – anything stored in computerized form is vulnerable to breakdown and obsolescence.”");
+
+    printer.newLine();
+
+    printer.alignRight();
+    printer.println("— Bruce Sterling (2004)");
+
+    printer.newLine();
+
+    printer.alignCenter();
+
+    printer.newLine();
 
     await printer.printImage(image);
+
+    printer.newLine();
+
+    printer.println("No refunds bye");
+
+    printer.newLine();
+
+    await printer.printImage("./ddd.png");
 
     printer.cut();
     // printer.clear();
@@ -74,6 +119,8 @@ async function setup() {
   // const client = setupSanity();
   const printer = await setupPrinter();
 
+  let counter = 10001;
+
   // Initialize watcher.
   const watcher = chokidar.watch(process.env.FOLDER, {
     ignored: /(^|[\/\\])\../, // ignore dotfiles
@@ -84,7 +131,9 @@ async function setup() {
   watcher.on('add', async (path) => {
     console.log(chalk.green(`file added at path: ${path}`));
 
-    await printImage(printer, path);
+    await printImage(printer, path, counter);
+
+    counter++;
     // sendToServer(client, path);
   });
 }
